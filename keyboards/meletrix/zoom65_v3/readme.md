d87ab278fefa89d0752c59df88433b85ed491455
ozwaldorf <self@ossian.dev>
2024-12-11 21:05:16 -0500

    feat(meletrix): zoom65_v3

---
+++ keyboards/meletrix/zoom65_v3/readme.md
@@ -0,0 +1,19 @@
+# Zoom65 v3 keyboard
+
+The most customizable 65% keyboard.
+
+* Keyboard Maintainer: @ozwaldorf
+* Hardware Supported: Zoom65 v3 1.6mm pcb
+* Hardware Availability: [monsgeek](https://www.monsgeek.com/)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make meletrix/zoom65_v3:default
+
+Flashing example for this keyboard:
+
+    make meletrix/zoom65_v3:default:flash
+
+**Reset Key**: Hold down the key located at *K00*, which programmed as *Esc* while plugging in the keyboard.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ keyboards/meletrix/zoom65_v3/zoom65_v3.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2023 jonylee@hfd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+
+enum __layers { WIN_B, WIN_FN };
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) {
+        return false;
+    }
+    switch (keycode) {
+        case DF(WIN_B):
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(WIN_B);
+                layer_state_set(1 << WIN_B);
+            }
+            return false;
+
+        default:
+            return true;
+    }
+}
