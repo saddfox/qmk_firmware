5dbb3a66687924a0ac2e5c5d73b4ace8b3cead53
ozwaldorf <self@ossian.dev>
2024-12-16 07:34:59 -0500

    refactor(meletrix/zoom65v3): separate ansi revision

--- keyboards/meletrix/zoom65_v3/ansi/keymaps/oz/keymap.c
+++ keyboards/meletrix/zoom65_v3/ansi/keymaps/oz/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
         _______, BT_PRF1, BT_PRF2, BT_PRF3, OU_2P4G, OU_USB , _______, _______, KC_INS,  _______, KC_PSCR, _______, SC_TOGG, UG_NEXT, SC_UP,
         _______, BT_PRF4, BT_PRF5, _______, _______, _______, _______, _______, _______, UG_TOGG, _______, _______,          UG_HUEU, SC_DOWN,
-        _______,          _______, _______, KC_CALC, _______, QK_BOOT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, UG_VALU, SC_SWCH,
+        _______,          _______, _______, EE_CLR,  _______, QK_BOOT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, UG_VALU, SC_SWCH,
         _______, GU_TOGG, _______,                   _______,                                     _______, _______, UG_SATD, UG_VALD, UG_SATU),
 
 };
