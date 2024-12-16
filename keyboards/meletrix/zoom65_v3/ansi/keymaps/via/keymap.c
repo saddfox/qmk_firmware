5cec6c36ac6f02ecd6057d4e1a5adcfdfad9b852
ozwaldorf <self@ossian.dev>
2024-12-16 14:13:18 -0500

    fix(meletrix/zoom65_v3): default bindings now use windows layout

--- keyboards/meletrix/zoom65_v3/ansi/keymaps/via/keymap.c
+++ keyboards/meletrix/zoom65_v3/ansi/keymaps/via/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS, KC_DEL,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,  KC_PGUP,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RALT, MO(WIN_FN), KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, MO(WIN_FN), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_FN] = LAYOUT( /* Fn */
         EE_CLR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  SC_TOGG,  _______,
@@ -17,5 +17,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, BT_PRF4, BT_PRF5, _______, _______, _______, _______, _______, _______, UG_TOGG, _______, _______,          UG_HUEU, SC_DOWN,
         _______, _______, _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, UG_VALU, SC_SWCH,
         _______, GU_TOGG, _______,                   _______,                                     _______, _______, UG_SATD, UG_VALD, UG_SATU),
-
 };
