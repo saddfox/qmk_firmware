52a7fa0daa310c2d59c1b46a6b10b95a825c2dd4
ozwaldorf <self@ossian.dev>
2024-12-17 20:52:17 -0500

    chore(meletrix/zoom65_v3): tweak brightness more, update release.sh

--- release.sh
+++ release.sh
@@ -5,14 +5,16 @@ VERSION=$(nix run nixpkgs\#jq -- -r .usb.device_version keyboards/meletrix/zoom6
 DIR="firmware-v$VERSION"
 
 echo ">> Building zoom65v3 firmware (version $VERSION)"
-make meletrix/zoom65_v3/ansi:via meletrix/zoom65_v3/iso:via
 
-echo ">> Bundling release directory"
-[[ -f "$DIR" ]] && rm $DIR -r
-mkdir -p $DIR
+make meletrix/zoom65_v3/ansi:via
+
+# clear and init dir
+[[ -f "$DIR" ]] && rm $DIR -r; mkdir -p $DIR
 
 mv meletrix_zoom65_v3_ansi_via.bin $DIR/meletrix_zoom65_v3_ansi_via_$VERSION.bin
 cp keyboards/meletrix/zoom65_v3/ansi/via.json $DIR/meletrix_zoom65_v3_ansi_via_$VERSION.json
+
+make meletrix/zoom65_v3/iso:via
 
 mv meletrix_zoom65_v3_iso_via.bin $DIR/meletrix_zoom65_v3_iso_via_$VERSION.bin
 cp keyboards/meletrix/zoom65_v3/iso/via.json $DIR/meletrix_zoom65_v3_iso_via_$VERSION.json
